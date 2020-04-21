052ffaf593d0bc7807cf185a8452aac983bf5678
luisquintanilla <Luis.Quintanilla@microsoft.com>
2020-04-20 21:27:51 -0400

    Initial commit

---
+++ ASLModelBuilderAPI/Controllers/PredictionController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using ASLModelBuilderML.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+
+namespace ASLModelBuilderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredictionController : ControllerBase
+    {
+        private readonly PredictionEngine<ModelInput, ModelOutput> _predictionEngine;
+        private readonly object _predictionEngineLock = new object();
+
+        public PredictionController(PredictionEngine<ModelInput, ModelOutput> predictionEngine)
+        {
+            _predictionEngine = predictionEngine;
+        }
+
+        [HttpPost]
+        public async Task<ModelOutput> ClassifyImage([FromBody] Dictionary<string, string> input)
+        {
+            ModelOutput prediction;
+            
+            // Get raw image bytes
+            var imageBytes = Convert.FromBase64String(input["data"]);
+
+            string imagePath = Path.Join(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), $"{imageBytes.GetHashCode()}.jpeg");
+
+            using (var ms = new MemoryStream(imageBytes))
+            {
+                // Save the image to a file
+                using (var img = await Task.Run(() => Image.FromStream(ms)))
+                    await Task.Run(() => img.Save(imagePath));
+            }
+
+            lock (_predictionEngineLock)
+            {
+                // Use Prediction to classify image
+                prediction = _predictionEngine.Predict(new ModelInput { ImageSource = imagePath });
+            }
+
+            return prediction;
+        }
+    }
+}
---
+++ ASLModelBuilderWASM/wwwroot/js/interop.js
@@ -0,0 +1,34 @@
+﻿// Start getting data
+window.Initialize = async (elements) => {
+
+    function handleSuccess(stream) {
+        window.stream = stream;
+        document.getElementById(elements['video']).srcObject = stream;
+    }
+
+    try {
+        let stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: { width: 640, height: 480 } });
+        handleSuccess(stream);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+//DrawImage
+window.Snap = async (src, dest) => {
+    let video = document.getElementById(src);
+    let ctx = get2DContext(dest);
+    ctx.drawImage(video, 0, 0, 640, 480);
+}
+
+// Get image as base64 string
+window.GetImageData = async (el, format) => {
+    let canvas = document.getElementById(el);
+    let dataUrl = canvas.toDataURL(format)
+    return dataUrl.split(',')[1];
+}
+
+// Helper functions
+function get2DContext(el) {
+    return document.getElementById(el).getContext('2d');
+}
